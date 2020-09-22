bff4812263b1efa2f76c56f8a3597bcd6ff57a89
David van der Westhuizen <dcjwest@gmail.com>
2020-09-22 20:56:35 +0200

    Updates

---
+++ precache-manifest.a5ed153926ef50cb7a5c190fc135777c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f0a9a7cbd5c0db2d17477e479a37df",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "55ce59ecb697ed541a65",
+    "url": "/hangman/static/css/main.7bd66709.chunk.css"
+  },
+  {
+    "revision": "2986ab771b96822977c7",
+    "url": "/hangman/static/js/2.c740e2da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.c740e2da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55ce59ecb697ed541a65",
+    "url": "/hangman/static/js/main.e6288ccd.chunk.js"
+  },
+  {
+    "revision": "7ef95293683d0ed87720",
+    "url": "/hangman/static/js/runtime-main.6b115b9b.js"
+  }
+]);
