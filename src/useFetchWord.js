beffcf51e6dd49186c92b5471ad8d935cf79e27c
David van der Westhuizen <dcjwest@gmail.com>
2020-10-09 14:35:01 +0200

    Random Word API broken/unavailable - modified useFetchWord hook to use random words from hardcoded array of strings.

--- src/useFetchWord.js
+++ src/useFetchWord.js
@@ -1,31 +1,40 @@
 import { useState, useEffect } from 'react';
 import { randomWords } from './randomWords';
 
-const PROXY = 'https://cors-anywhere.herokuapp.com/';
-
-const API_URL = `${PROXY}https://random-word-api.herokuapp.com/word`;
-
 export default function useFetchWord(replayCheck, setReplay) {
     const [selectedWord, setSelectedWord] = useState('');
-    const [error, setError] = useState(null);
+
+    
 
     useEffect(() => {
-        const fetchWord = async () => {
-            try {
-                const res = await fetch(API_URL);
-                const data = await res.json();
-                setSelectedWord(data[0].toUpperCase());
-            } catch (err) {
-                setError(err);
+        /*  BROKEN: Random Word API not available.
+            TOD0: Create own API .
+        */
+        // const fetchWord = async () => {
+        //     const PROXY = 'https://cors-anywhere.herokuapp.com/';
+        //     const API_URL = `${PROXY}https://random-word-api.herokuapp.com/word`;
+        //     try {
+        //         const res = await fetch(API_URL);
+        //         const data = await res.json();
+        //         setSelectedWord(data[0].toUpperCase());
+        //     } catch (err) {
+        //         setError(err);
 
-                let randomIdx = Math.floor(Math.random() * randomWords.length);
-                setSelectedWord(randomWords[randomIdx].toUpperCase());
-            }
+        //         let randomIdx = Math.floor(Math.random() * randomWords.length);
+        //         setSelectedWord(randomWords[randomIdx].toUpperCase());
+        //     }
+        // };
+
+        /* Get word using hard-coded strings from randomWords.js */
+        const fetchWord = () => {
+            let randomIdx = Math.floor(Math.random() * randomWords.length);
+            setSelectedWord(randomWords[randomIdx].toUpperCase());
         };
+
         if (replayCheck || replayCheck === undefined) {
             fetchWord();
             setReplay(false);
         }
     }, [replayCheck, setReplay]);
-    return { selectedWord, error };
+    return { selectedWord };
 }
